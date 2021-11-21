38d41a02c49fff9bbf5dab9ba0806f01e9e74f1b
thaont121 <thaonguyen.201.9b@gmail.com>
2021-11-22 00:19:22 +0700

    add again function login

--- clothing-shop-website/Infrastructure/Persistent/Repository/AccountsRepository.cs
+++ clothing-shop-website/Infrastructure/Persistent/Repository/AccountsRepository.cs
@@ -27,7 +27,6 @@ namespace Infrastructure.Persistent.Repository
         {
             return _dbContext.Accounts.FirstOrDefault(p => p.Id == accountID && p.State > 0);
         }
-
         public Account CreateAccount(Account account, Customer customer, Staff staff)
         {
             try
@@ -77,5 +76,6 @@ namespace Infrastructure.Persistent.Repository
             Account account = await _dbContext.Accounts.FirstOrDefaultAsync(u => u.Email == email && u.Password == password);
             return account;
         }
+
     }
 }
